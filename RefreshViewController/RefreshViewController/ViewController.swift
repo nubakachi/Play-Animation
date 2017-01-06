178f3815c314460dc5db1a440c52ef901d94ea45
shrawan2015 <shrawan25.sharma@gmail.com>
2017-01-06 19:52:27 +0530

    Update in refresh View

--- RefreshViewController/RefreshViewController/ViewController.swift
+++ RefreshViewController/RefreshViewController/ViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
 
+    var dataArray: Array<String> = ["One", "Two", "Three", "Four", "Five"]
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    var customView:UIView!
+    
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        configureTableView()
+        addRefreshControl()
+        loadViewFromXib()
+    }
+    
+    func addRefreshControl() {
+        refreshControl = UIRefreshControl(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 300) )
+        
+        refreshControl.backgroundColor = .red
+        refreshControl.tintColor = .blue
+    }
+    
+    
+    func loadViewFromXib() {
+        
+        let refreshContents = Bundle.main.loadNibNamed("RefreshView", owner: self, options: nil)
+        let extractView = refreshContents?.first as! UIView
+        customView = UIView(frame: self.refreshControl.bounds)
+        customView = extractView
+        refreshControl.addSubview(customView)
+        tblView.insertSubview(refreshControl, at: 0)
+
+        
+        
+    }
+    
+    func configureTableView(){
+        tblView.register(UITableViewCell.self, forCellReuseIdentifier: "RefreshCell")
+        tblView.delegate = self
+        tblView.dataSource = self
+    }
+  
+   
+    
+    //MARK:- Table view data source and delegate
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblView.dequeueReusableCell(withIdentifier: "RefreshCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "Text Vuiew"
+         return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
-
-
+    
+    
 }
 
